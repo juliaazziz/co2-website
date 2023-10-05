3d6ebb1b93479bf4a8b758ebbd2e9a03eb798508
julia <julia.azziz@fing.edu.uy>
2023-10-05 16:24:02 -0300

    mejoras

--- connect_btn.js
+++ connect_btn.js
@@ -3,69 +3,71 @@ const MAX_POINTS = 100;
 const regex = /C(\d+)T(\d+)/;
 
 const connectButton = document.getElementById('connect_btn');
-let port;
 
-/* Variable donde guardo la gráfica */
+/* Variable con la gráfica */
 const dataChart = new Chart(
     document.getElementById('chart'),
     {
-    type: 'line',
-    options: {
-        plugins: {
-            legend: {
-                labels: {
-                    font: {
-                        size: 17
+        type: 'line',
+        options: {
+            plugins: {
+                legend: {
+                    labels: {
+                        font: {
+                            size: 17
+                        }
+                    }
+                }
+            },
+            responsive: true,
+            stacked: false,
+            scales: {
+                y: {
+                    type: 'linear',
+                    display: true,
+                    position: 'left',
+                },
+                y1: {
+                    type: 'linear',
+                    display: true,
+                    position: 'right',
+
+                    grid: {
+                        drawOnChartArea: false,
                     }
                 }
             }
         },
-        responsive: true,
-        stacked: false,
-        scales: {
-            y: {
-              type: 'linear',
-              display: true,
-              position: 'left',
-            },
-            y1: {
-              type: 'linear',
-              display: true,
-              position: 'right',
-      
-              // grid line settings
-              grid: {
-                drawOnChartArea: false, // only want the grid lines for one axis to show up
-              }
-            }
+        data: {
+            labels: [],
+            datasets: [
+                {
+                    label: 'Concentración de CO2 (ppm)',
+                    data: [],
+                    yAxisID: 'y'
+                },
+                {
+                    label: 'Temperatura (°C)',
+                    data: [],
+                    yAxisID: 'y1'
+                }
+            ]
         }
-    },
-    data: {
-        labels: [],
-        datasets: [
-        {
-            label: 'Concentración de CO2 (ppm)',
-            data: [],
-            yAxisID: 'y'
-        },
-        {
-            label: 'Temperatura (°C)',
-            data: [],
-            yAxisID: 'y1'
-        }
-        ]
-    }
     }
 );
 
 /* Funciones para conexión serial */
 
+let port;
+
 async function connectToSerialPort() {
     try {
-        /* Queda esperando al puerto */
+        /* Conecta al puerto elegido */
         port = await navigator.serial.requestPort();
         await port.open({ baudRate: BAUDRATE });
         console.log(`Conectado a ${port.getInfo().usbProductId}`);
+
+        /* Queda leyendo datos */
         getReader();
     } catch (err) {
         console.error('Error:', err);
@@ -74,6 +76,7 @@ async function connectToSerialPort() {
 
 connectButton.addEventListener('click', connectToSerialPort);
 
+/* Lee datos cuando haya */
 async function getReader() {
     setTimeout(() => {
         if (port && port.readable) {
@@ -82,41 +85,41 @@ async function getReader() {
     }, 1500);
 }
 
-/* Función auxiliar para leer buffer y guardar datos */
+/* Función auxiliar para leer buffer que devuelve string con el dato */
 async function readInto(reader, buffer) {
     let offset = 0;
-    let buf_str = "";
+    let data = "";
     let str;
 
+    /* El nodo envía 17 o 18 bytes */
     while ((offset < 18) && (str != "\r")) {
-        const { value, done } = await reader.read( new Uint8Array(buffer));
+        const { value, done } = await reader.read(new Uint8Array(buffer));
         if (done) {
             break;
         }
         buffer = value.buffer;
-        str = new TextDecoder().decode(buffer);
         offset += value.byteLength;
-        buf_str += new TextDecoder().decode(buffer);
+        str = new TextDecoder().decode(buffer);
+        data += str;
     }
-    return buf_str;
+    return data;
 }
 
 /* Leo datos, parseo y actualizo gráfica */
-async function readFromUART() {    
+async function readFromUART() {
+    let data_str, match, co2, temp;
     const reader = port.readable.getReader({ mode: "byob" });
-    let data_str, match, co2, temp; 
-    
-    while (true) {
-        let buffer = new ArrayBuffer(1);
-        data_str = await readInto(reader, buffer);
-        console.log(data_str);
 
-        /* Paso a string y busco datos */
+    while (true) {
+        data_str = await readInto(reader, new ArrayBuffer(1));
+
+        /* Busco datos */
         match = data_str.match(regex);
         if (match) {
             co2 = parseInt(match[1], 10);
             temp = parseInt(match[2], 10);
 
+            /* Grafico nuevos datos */
             dataChart.data.labels.push(new Date().toLocaleTimeString());
             dataChart.data.datasets[0].data.push(co2);
             dataChart.data.datasets[1].data.push(temp);
@@ -126,6 +129,7 @@ async function readFromUART() {
             if (dataChart.data.datasets[0].data.length > MAX_POINTS) {
                 dataChart.data.labels.shift();
                 dataChart.data.datasets[0].data.shift();
+                dataChart.data.datasets[1].data.shift();
             }
         }
     }
